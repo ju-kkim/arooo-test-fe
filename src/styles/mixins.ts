804202d5d696250c1a04337e2735fedba40de6e9
ju-kkim <wndus8198@naver.com>
2023-08-01 01:09:41 +0900

    feat: flex mixin 함수

--- src/styles/mixins.ts
+++ src/styles/mixins.ts
@@ -11,3 +11,28 @@ export const typography = ({ size, weight = 'regular' }: Typography) => {
     font-weight: ${FONT[size].weight[weight]};
   `;
 };
+
+type Flex = {
+  direction?: 'row' | 'row-reverse' | 'column' | 'column-reverse';
+  alignItems?: 'flex-start' | 'flex-end' | 'center' | 'stretch' | 'baseline';
+  justifyContent?: 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around';
+  gap?: string;
+  wrap?: 'nowrap' | 'wrap' | 'wrap-reverse';
+};
+
+export const flex = ({
+  direction = 'row',
+  alignItems = 'stretch',
+  justifyContent = 'flex-start',
+  gap = '0px',
+  wrap = 'nowrap',
+}: Flex) => {
+  return `
+    display: flex;
+    flex-direction: ${direction};
+    align-items: ${alignItems};
+    justify-content: ${justifyContent};
+    gap: ${gap};
+    flex-wrap: ${wrap};
+  `;
+};
